37d45695575aa9b99bc1b25f9a67f5dc3b9ca072
ohmari <omarimwakaribu92@gmail.com>
2019-11-27 17:26:43 +0300

    Initial commit

---
+++ src/main/java/org/techcaresinc/signup/SignupApplication.java
@@ -0,0 +1,13 @@
+package org.techcaresinc.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignupApplication.class, args);
+    }
+
+}
